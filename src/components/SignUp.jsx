4f1737979537788f13c9ffc2148a95632b1d44cc
Shantosifat1 <shanto201934024@gmail.com>
2025-06-15 02:04:23 +0600

    signup authentication done

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,10 +1,65 @@
-import React from "react";
-// import { Lottie } from "@lottiefiles/react-lottie-player";
-import signupAnimation from "../assets/register/Animation - 1749909840681.json"; // replace with your Lottie file
-import { Link } from "react-router";
+import React, { use, useState } from "react";
+import signupAnimation from "../assets/register/Animation - 1749909840681.json";
+import { Link, useNavigate } from "react-router";
 import Lottie from "lottie-react";
+import { AuthContext } from "../Provider/AuthProvider";
+import { FaGoogle } from "react-icons/fa";
 
 const Signup = () => {
+  const { createUser, googleSignIn } = use(AuthContext);
+  const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const photo = form.photo.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(name, email, password, photo);
+
+    // Password validation
+    // const hasUppercase = /[A-Z]/.test(password);
+    // const hasLowercase = /[a-z]/.test(password);
+    // const isLongEnough = password.length >= 6;
+
+    // if (!hasUppercase) {
+    //   alert("Password must contain at least one uppercase letter.");
+    //   return;
+    // }
+    // if (!hasLowercase) {
+    //   alert("Password must contain at least one lowercase letter.");
+    //   return;
+    // }
+    // if (!isLongEnough) {
+    //   alert("Password must be at least 6 characters long.");
+    //   return;
+    // }
+
+    // createUser
+
+    createUser(email, password)
+      .then((result) => {
+        console.log(result.user);
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  // google
+  const handleGoogleSignUp = () => {
+    googleSignIn()
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-tr from-rose-50 via-sky-50 to-indigo-100 p-4">
       <div className="w-full max-w-5xl bg-white shadow-2xl rounded-3xl overflow-hidden grid grid-cols-1 md:grid-cols-2">
@@ -27,12 +82,22 @@ const Signup = () => {
         {/* Right: Form */}
         <div className="p-8 md:p-10">
           <h2 className="text-3xl font-bold text-gray-800 mb-6">Sign Up</h2>
-          <form className="space-y-5">
+          <form onSubmit={handleSignUp} className="space-y-5">
             {/* Name */}
             <div>
               <label className="block mb-1 text-gray-600">Name</label>
               <input
                 type="text"
+                name="name"
+                className="w-full border text-black border-gray-300 rounded-xl px-4 py-2 focus:ring-2 focus:ring-indigo-300 outline-none"
+              />
+            </div>
+            {/* image */}
+            <div>
+              <label className="block mb-1 text-gray-600">Image</label>
+              <input
+                type="text"
+                name="photo"
                 className="w-full border text-black border-gray-300 rounded-xl px-4 py-2 focus:ring-2 focus:ring-indigo-300 outline-none"
               />
             </div>
@@ -42,6 +107,7 @@ const Signup = () => {
               <label className="block mb-1 text-gray-600">Email</label>
               <input
                 type="email"
+                name="email"
                 className="w-full border text-black border-gray-300 rounded-xl px-4 py-2 focus:ring-2 focus:ring-indigo-300 outline-none"
               />
             </div>
@@ -51,6 +117,7 @@ const Signup = () => {
               <label className="block mb-1 text-gray-600">Password</label>
               <input
                 type="password"
+                name="password"
                 className="w-full border text-black border-gray-300 rounded-xl px-4 py-2 focus:ring-2 focus:ring-indigo-300 outline-none"
               />
             </div>
@@ -61,6 +128,12 @@ const Signup = () => {
               className="w-full py-2 bg-indigo-600 hover:bg-indigo-700 text-white font-semibold rounded-xl transition duration-200"
             >
               Create Account
+            </button>
+            <button
+              onClick={handleGoogleSignUp}
+              className="btn btn-secondary btn-outline rounded-xl w-full my-1"
+            >
+              <FaGoogle size={18}></FaGoogle> SignUp with Google
             </button>
           </form>
 
